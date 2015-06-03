f9d6884d9e5de1feebcc55cd3858382752526423
ganmacs <ganmacs@gmail.com>
2015-06-03 11:42:58 +0900

    serial

---
+++ books/working_with_tcp_sockets/ftp/serial.rb
@@ -0,0 +1,46 @@
+require 'socket'
+require_relative './command_handler.rb'
+
+module FTP
+  class Serial
+    CRLF = "\r\n".freeze
+
+    def initialize(port = 21)
+      @control_socket = TCPServer.new(port)
+      trap(:INT) { exit }
+    end
+
+    def gets
+      @client.gets(CRLF)
+    end
+
+    def respond(message)
+      @client.write(message)
+      @client.write(CRLF)
+    end
+
+    def run
+      loop do
+        # new client connection
+        @client = @control_socket.accept
+        respond '220 OHAI'
+
+        handler = CommandHandler.new(self)
+
+        loop do
+          request = gets
+
+          if request
+            respond handler.handle(request)
+          else
+            @client.close
+            break
+          end
+        end
+      end
+    end
+  end
+end
+
+server = FTP::Serial.new(4481)
+server.run
