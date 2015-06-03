77d3d4f8805d80a054ecc15ea1c6afb87742032a
ganmacs <ganmacs@gmail.com>
2015-06-03 13:38:23 +0900

    process per connection

---
+++ books/working_with_tcp_sockets/ftp/process_per_connection.rb
@@ -0,0 +1,49 @@
+require 'socket'
+require_relative './command_handler.rb'
+
+module FTP
+  class ProcessPerConnection
+    CRLF = "\r\n"
+
+    def initialize(port = 21)
+      @control_socket = TCPServer.new(port)
+      trap(:INT) { exit }
+    end
+
+    def gets
+      @client.gets(CRLF)
+    end
+
+    def respond(message)
+      @client.write(message)
+      @client.write(CRLF)
+    end
+
+    def run
+      loop do
+        @client = @control_socket.accept
+        pid = fork do
+          respond '220 OHAI'
+          handler = CommandHandler.new(self)
+
+          loop do
+            request = gets
+
+            if request
+              respond handler.handle(request)
+            else
+              @client.close
+              break
+            end
+          end
+        end
+
+        # avoid zombie process?
+        Process.detach(pid)
+      end
+    end
+  end
+end
+
+server = FTP::ProcessPerConnection.new(4481)
+server.run
