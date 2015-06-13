16b73c3bb0a4c284c1f320bab27816e64d2943b4
ganmacs <ganmacs@gmail.com>
2015-06-13 23:56:57 +0900

    add listLexer and fix lexer name

--- scala/pat/src/lexer/lexer.scala
+++ scala/pat/src/lexer/lexer.scala
@@ -1,4 +1,19 @@
 package lexer
 
-abstract class Lexer {
+abstract class Lexer(val input: String) {
+  val EOF_TYPE = 1
+  val EOF = -1.toChar
+  var p = 0
+  var c = input.charAt(p)
+
+  def consume: Unit = {
+    p += 1
+    c = if (p >= input.length) EOF else input.charAt(p)
+  }
+
+  // match
+  def check(x: Char) = {
+    if (c != x) throw new Exception("expecting " + x + "; found" + c)
+    consume
+  }
 }
