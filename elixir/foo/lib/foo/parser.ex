6229b54ad0fde3e632c65071a1dfcf666adf5e62
ganmacs <ganmacs@gmail.com>
2015-06-28 19:45:13 +0900

    Extract Integer module which in parser.ex into paser/inteer.ex

--- elixir/foo/lib/foo/parser.ex
+++ elixir/foo/lib/foo/parser.ex
@@ -25,82 +25,11 @@ defmodule Foo.Parser do
   # def lookup("{" <> rest), do: parse_map(rest)
   def do_parse("[" <> rest), do: parse_array(rest)
   def do_parse(<<char, rest :: binary >> = string) when char in '-0123456789' do
-    parse_number(string)
+    Foo.Parser.Numbers.parse(string)
   end
 
   def parse_array("]") do
     []
-  end
-
-  # Numbers
-  def parse_number("-" <> rest) do
-    case rest do
-      "0" <> rest -> number_frac(rest, ["-0"])
-      rest -> number_int(rest, [?-])
-    end
-  end
-
-  def parse_number("0" <> rest) do
-    number_frac(rest, [?0])
-  end
-
-  def parse_number(string) do
-    number_int(string, [])
-  end
-
-  def number_int(<<char, _ :: binary >> = string, acc) when char in '123456789' do
-    {digit, rest} = number_digit(string)
-    number_frac(rest, [acc, digit])
-  end
-
-  def number_int(_, acc), do: raise(Foo.SyntaxError, message: "invalid number")
-
-  def number_frac("." <> rest, acc) do
-    {digit, rest} = number_digit(rest)
-    number_exp(rest, true, [acc, ?., digit])
-  end
-
-  def number_frac(string, acc) do
-    number_exp(string, false, acc)
-  end
-
-  def number_exp(<<e>> <> rest, frac, acc) when e in 'eE' do
-    e = if frac, do: ?e, else: ".0e"
-
-    case rest do
-      "-" <> rest -> number_exp_continue(rest, [acc, e, ?-])
-      "+" <> rest -> number_exp_continue(rest, [acc, e])
-      rest -> number_exp_continue(rest, [acc, e])
-    end
-  end
-
-  def number_exp(string, frac, acc) do
-    generate_number([acc], frac)
-  end
-
-  def number_exp_continue(rest, acc) do
-    {digits, rest} = number_digit(rest)
-    generate_number([acc, digits], true)
-  end
-
-  def number_digit(<<char, rest :: binary>> = string) when char in '0123456789' do
-    size = digit_size(rest, 1)
-    <<digit :: binary-size(size), rest :: binary>> = string
-    {digit, rest}
-  end
-
-  def digit_size(<<char, rest :: binary>>, acc) when char in '1234567890'do
-    digit_size(rest, acc + 1)
-  end
-
-  def digit_size(_, acc), do: acc
-
-  def generate_number(iolist, false) do
-    IO.iodata_to_binary(iolist) |> String.to_integer
-  end
-
-  def generate_number(iolist, true) do
-    IO.iodata_to_binary(iolist) |> String.to_float
   end
 
   # def skip_whitespace(<<char>>), do: string
---
+++ elixir/foo/lib/foo/parser/numbers.ex
@@ -0,0 +1,89 @@
+defmodule Foo.Parser.Numbers do
+  @moduledoc """
+  123.123e+21
+  |   |  |__ exp
+  |   |__ frac
+  |____ digits
+
+  """
+
+  def parse("-" <> rest) do
+    case rest do
+      "0" <> rest -> frac(rest, ["-0"])
+      rest -> int(rest, [?-])
+    end
+  end
+
+  def parse("0" <> rest) do
+    frac(rest, [?0])
+  end
+
+  def parse(string) do
+    int(string, [])
+  end
+
+  @doc """
+  accpet 1-9 char
+  """
+  def int(<<char, _ :: binary >> = string, acc) when char in '123456789' do
+    {digit, rest} = digits(string)
+    frac(rest, [acc, digit])
+  end
+
+  def frac("." <> rest, acc) do
+    {digit, rest} = digits(rest)
+    exp(rest, true, [acc, ?., digit])
+  end
+
+  def frac(string, acc) do
+    exp(string, false, acc)
+  end
+
+  def exp(<<e>> <> rest, frac, acc) when e in 'eE' do
+    e = if frac, do: ?e, else: ".0e"
+
+    case rest do
+      "-" <> rest -> exp_continue(rest, [acc, e, ?-])
+      "+" <> rest -> exp_continue(rest, [acc, e])
+      rest -> exp_continue(rest, [acc, e])
+    end
+  end
+
+
+  def exp_continue(rest, acc) do
+    {digits, rest} = digits(rest)
+    generate_number([acc, digits], true)
+  end
+
+
+  @doc """
+  When not has exp
+  """
+  def exp(string, frac, acc) do
+    generate_number([acc], frac)
+  end
+
+  @doc """
+  parse string to digit and anthor part
+  digits("1234.1") #=> {"1234", ".1"}
+  """
+  def digits(<<char>> <> rest = string) when char in '0123456789' do
+    size = digits_size(rest, 1)
+    <<digit :: binary-size(size), rest :: binary>> = string
+    {digit, rest}
+  end
+
+  def digits_size(<<char>> <> rest, acc) when char in '0123456789'do
+    digits_size(rest, acc + 1)
+  end
+
+  def digits_size(_, acc), do: acc
+
+  def generate_number(iolist, false) do
+    IO.iodata_to_binary(iolist) |> String.to_integer
+  end
+
+  def generate_number(iolist, true) do
+    IO.iodata_to_binary(iolist) |> String.to_float
+  end
+end
