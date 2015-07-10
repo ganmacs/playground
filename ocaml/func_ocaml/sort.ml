5f71ef6038c4aa4e9ffba39eb28b6138ad418091
ganmacs <ganmacs@gmail.com>
2015-07-11 07:59:23 +0900

    Complete sort

--- ocaml/func_ocaml/sort.ml
+++ ocaml/func_ocaml/sort.ml
@@ -7,3 +7,27 @@ let rec insert_sort lst =
      | x :: xs -> insert x (insert_sort xs)
 ;;
 insert_sort [9; 1; 8; 2; 7; 3; 6; 4; 5] ;;
+
+let rec quick_sort = function
+  | [] -> []
+ | x :: xs -> let (l, r) = List.partition (fun y -> x > y) xs in
+              quick_sort l @ [x] @ quick_sort r
+;;
+
+quick_sort [5; 3; 7; 6; 9; 8; 1; 2; 4];;
+
+(* tail recur ver. *)
+
+let rec partition f l r = function
+  | [] -> (l, r)
+  | x :: xs -> if (f x)
+               then partition f (x :: l) r xs
+               else partition f l (x :: r) xs
+;;
+
+let rec quick_sort = function
+  | [] -> []
+ | x :: xs -> let (l, r) = partition (fun y -> x > y) [] [] xs in
+              quick_sort l @ [x] @ quick_sort r
+;;
+quick_sort [5; 3; 7; 6; 9; 8; 1; 2; 4];;
