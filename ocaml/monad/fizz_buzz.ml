db60b1a99681988202fa223fe8f4e9dabe4935a5
ganmacs <ganmacs@gmail.com>
2015-07-20 00:08:03 +0900

    wip fizz buzz

---
+++ ocaml/monad/fizz_buzz.ml
@@ -0,0 +1,30 @@
+module type Monad = sig
+  type 'a m
+  val return : 'a -> 'a m
+  val (>>=) : 'a m -> ('a -> 'b m) -> 'b m
+end
+
+module FizzBuzzM: Monad = struct
+  type 'a m = FB of 'a * string
+  let return x = FB (x, "")
+  let bind (FB (i, s)) f = let FB (j, s2) = f i in FB (j, s ^ s2)
+  let (>>=) = bind
+end
+
+module FizzBuzz = struct
+  include FizzBuzzM
+
+  type 'a fb = FB of 'a * string
+
+  let fizz x = if x mod 3 = 0 then FB (x, "fizz") else FB (x, "")
+  let buzz x = if x mod 5 = 0 then FB (x, "buzz") else FB (x, "")
+
+  let fizzbuzz x = (fizz x)
+
+  let print = function
+    | FB (x, "") -> print_int x
+    | FB (x, s) -> print_string s
+end
+;;
+
+FizzBuzz.print @@ FizzBuzz.fizzbuzz 15
