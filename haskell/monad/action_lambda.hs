ca2c6ca75a3a8d3f18fa7480c5fce619e47c90b0
ganmacs <ganmacs@gmail.com>
2015-07-22 17:38:46 +0900

    tanosii

---
+++ haskell/monad/action_lambda.hs
@@ -0,0 +1,43 @@
+-- http://qiita.com/7shi/items/4a8a2807bb5186576c61
+
+import Control.Monad
+import System.Random
+import Data.IORef
+import Data.Array.IO
+
+randomPlus1 = do
+  a <- newIORef =<< (getStdRandom $ randomR (0, 9) :: IO Int)
+  modifyIORef a (+1)
+  print =<< readIORef a
+
+
+modifyArray ary i f = writeArray ary i =<< f <$> readArray ary i
+fixArray = do
+  a <- newArray (0, 2) 0 :: IO (IOUArray Int Int)
+  print =<< getElems a
+  modifyArray a 1 (+1)
+  print =<< getElems a
+
+dice :: IO Int
+dice = getStdRandom $ randomR (1, 6)
+
+replica5 :: Monad m => m a -> m [a]
+replica5 = replicateM 5
+
+printReplica = do
+  print =<< replica5 (return 4)
+  print =<< replica5 dice
+
+printReplica' = replicateM_ 3 $ print =<< dice
+
+printWithFor = do
+  a <- forM [1 .. 3] $ \x -> do print x
+                                return x
+  print a
+
+-- when' = do
+--   r <- dice
+--   print r
+--   when (r /= 3) main
+
+-- main =
--- haskell/monad/do.hs
+++ haskell/monad/do.hs
@@ -8,6 +8,26 @@ hoge x = do
   x <- div2 $ x
   return x
 
+printHello = do
+  print "hello"
+  print "hello"
+
+printHelloWithDo = print "hello" >>= \_ -> print "hello"
+
+pointFree x =
+  return x >>= print
+
+ppointFree x = do
+  k <- return x
+  print k
+
+pppointFree x = print =<< return x
+
 main :: IO ()
 main = do
-  print $ hoge 15
+  pppointFree 2
+  -- pointFree 10
+  -- printHelloWithDo
+  -- print $ hoge 15
+  -- printHello
+  -- print "hoge"
