0b4603f592f9429b12a8166906df586550d4c3c3
ganmacs <ganmacs@gmail.com>
2015-11-12 20:21:03 +0900

    implemtn config file loading and attach host by itself hostname

--- clojure/cpg/project.clj
+++ clojure/cpg/project.clj
@@ -3,5 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main cpg.yesmad)
+  :dependencies [[org.clojure/clojure "1.7.0"]])
--- clojure/cpg/src/cpg/yesmad.clj
+++ clojure/cpg/src/cpg/yesmad.clj
@@ -1,5 +1,7 @@
 (ns cpg.yesmad
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojure.java.shell :refer [sh]]))
 
 (defprotocol ConfigFile
   (slurp* [_]))
@@ -15,17 +17,52 @@
   (slurp* [file] (safe-slurp file (str {})))
 
   java.net.URL
-  (slurp* [url] (safe-slurp url (str {}))))
+  (slurp* [url] (safe-slurp url (str {})))
 
-(defn read-config [file-or-resource]
+  java.lang.String
+  (slurp* [s] (safe-slurp s)))
+
+(defn- get-hostname []
+  (.trim (:out (sh "hostname"))))
+
+(defn- read-config-file [file-or-resource]
+  (slurp* file-or-resource))
+
+(defn- update-config [current-config target-key from-key selector value]
+  (let [{new-upstream-config :config} (get current-config from-key)]
+    (assoc current-config
+           target-key
+           {:config (get-in new-upstream-config [selector value])})))
+
+(defn- load-edn-file [file-or-resource]
+  (edn/read-string (read-config-file file-or-resource)))
+
+(defn- attach-location [configs]
+  (assoc configs
+         :location {:yesmad/hostname (get-hostname)}))
+
+(defn- attach-config [config-map config-file]
+  (assoc config-map
+         :config (load-edn-file config-file)))
+
+(defn load-config [file-or-resource]
   (let [config-map {:general {:config-file file-or-resource}}]
-    (slurp* file-or-resource)))
+    (do
+      (println (-> config-map
+                   (update-in [:general] attach-config (get-in config-map [:general :config-file]))
+                   (update-config :host :general :nomad/hosts (get-hostname))
+                   attach-location
+                   ;; (expand-host-variable)
+                   ))
+      (read-config-file (get-in config-map [:general :config-file])))))
 
 (defmacro defconfig [name file-or-resource]
-  `(defn ~name []
-     (read-config ~file-or-resource)))
-
+  `(let [cached-config# (atom nil)]
+     (defn ~name []
+       (swap! cached-config#
+              (fn [f#]
+                ;; TODO use f#
+                (load-config ~file-or-resource))))))
+;; -- api
 (defconfig my-config (io/resource "server.edn"))
-
-(def -main []
-  (println "does not work!!"))
+(my-config)
