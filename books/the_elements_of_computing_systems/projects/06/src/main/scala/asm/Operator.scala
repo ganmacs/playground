de4dc4785600c15b4fec992814c6b215dd81ff8a
ganmacs <ganmacs@gmail.com>
2016-01-09 12:35:18 +0900

    Add operator

---
+++ books/the_elements_of_computing_systems/projects/06/src/main/scala/asm/Operator.scala
@@ -0,0 +1,74 @@
+package asm
+
+sealed trait Operator {
+  def asm: String
+}
+
+case class AOperator(v: String) extends Operator {
+  lazy val binaryV = Integer.parseInt(v).toBinaryString
+  override def asm: String = ("0000000000000000" + binaryV).takeRight(16)
+}
+
+case class COperator(dest: Option[String], comp: String, jump: Option[String]) extends Operator {
+  override def asm: String = "111" + Code.comp(comp) + ((dest, jump) match {
+    case (None, None) => Code.dest("null") + Code.jump("null")
+    case (Some(d), None) => Code.dest(d) + Code.jump("null")
+    case (None, Some(j)) => Code.dest("null") + Code.jump(j)
+    case (Some(d),  Some(j)) => Code.dest(d) + Code.jump(j)
+  })
+}
+
+object Code {
+  def dest(str: String) = str match {
+    case "null" => "000"
+    case "M"    => "001"
+    case "D"    => "010"
+    case "MD"   => "011"
+    case "A"    => "100"
+    case "AM"   => "101"
+    case "AD"   => "110"
+    case "AMD"  => "111"
+  }
+
+  def jump(str: String) = str match {
+    case "null" => "000"
+    case "JGT"  => "001"
+    case "JEQ"  => "010"
+    case "JGE"  => "011"
+    case "JLT"  => "100"
+    case "JNE"  => "101"
+    case "JLE"  => "110"
+    case "JMP"  => "111"
+  }
+
+  def comp(str: String) = str match {
+    case "0"   => "0101010"
+    case "1"   => "0111111"
+    case "-1"  => "0111010"
+    case "D"   => "0001100"
+    case "A"   => "0110000"
+    case "M"   => "1110000"
+    case "!D"  => "0001101"
+    case "!A"  => "0110001"
+    case "!M"  => "1110001"
+    case "-D"  => "0001111"
+    case "-A"  => "0110011"
+    case "-M"  => "1110011"
+    case "D+1" => "0011111"
+    case "A+1" => "0110111"
+    case "M+1" => "1110111"
+    case "D-1" => "0001110"
+    case "A-1" => "0110010"
+    case "M-1" => "1110010"
+    case "D+A" => "0000010"
+    case "D+M" => "1000010"
+    case "D-A" => "0010011"
+    case "D-M" => "1010011"
+    case "A-D" => "0000111"
+    case "M-D" => "1000111"
+    case "D&A" => "0000000"
+    case "D&M" => "1000000"
+    case "D|A" => "0010101"
+    case "D|M" => "1010101"
+  }
+}
---
+++ books/the_elements_of_computing_systems/projects/06/src/test/scala/asm/OperatorTest.scala
@@ -0,0 +1,34 @@
+package asm
+
+import org.scalatest.FunSpec
+import asm._
+
+class OperatorTest extends FunSpec {
+  describe("AOprator")  {
+    it("INT") {
+      assert(AOperator("1024").asm ==  "00010000000000")
+      assert(AOperator("1").asm ==  "00000000000001")
+    }
+  }
+
+  describe("COprator")  {
+    it("comp") {
+      assert(COperator(None, "M", None).asm ==  "1111110000000000")
+      assert(COperator(None, "1", None).asm ==  "1110111111000000")
+    }
+
+    it("dest = comp") {
+      assert(COperator(Some("D"), "M", None).asm ==  "1111110000010000")
+      assert(COperator(Some("M"), "0", None).asm ==  "1110101010001000")
+      assert(COperator(Some("M"), "1", None).asm ==  "1110111111001000")
+      assert(COperator(Some("M"), "D+M", None).asm ==  "1111000010001000")
+      assert(COperator(Some("M"), "M+1", None).asm ==  "1111110111001000")
+      assert(COperator(Some("D"), "D+A", None).asm ==  "1110000010010000")
+    }
+
+    it("comp;dest") {
+      assert(COperator(None, "0", Some("JMP")).asm ==  "1110101010000111")
+      assert(COperator(None, "D", Some("JGT")).asm ==  "1110001100000001")
+    }
+  }
+}
