5286de7affdea31bffdc0c6258f1b942abde8123
ganmacs <ganmacs@gmail.com>
2016-01-20 23:43:36 +0900

    now

--- books/the_elements_of_computing_systems/projects/tecs/src/main/scala/tecs/compiler/CompilationEngine.scala
+++ books/the_elements_of_computing_systems/projects/tecs/src/main/scala/tecs/compiler/CompilationEngine.scala
@@ -21,24 +21,65 @@ class CompilationEngine(input: String) {
 
   }
 
-  def compileClass()(lst: List[Syntax]): S_class = tokens match {
-    case S_keyword("class") :: (a@S_ident(_)) :: S_symbol("{") :: xs => compileClassVarDec(xs) match {
-      case (b@S_classVarDec(_, _, _, _)) :: xs => compileSubroutine(xs) match {
-        case (c@S_subroutineDec(_)) :: xs => xs match {
-          case S_symbol("}") :: Nil =>  S_class(a, b, c)
-          case x =>  throw new Exception(x.toString)
+  def compileClass(lst: List[Syntax]): Either[String, S_class] = tokens match {
+    case S_keyword("class") :: (a@S_ident(_)) :: S_symbol("{") :: xs =>
+      compileClassVarDec(xs) match {
+        case Right((clasVarDec, xs)) =>
+          compileSubroutine(xs) match {
+            case Right((subroutine, xs)) => xs match {
+              case S_symbol("}") :: Nil => Right(S_class(a, clasVarDec, subroutine))
+              case x => Left("class definition's } is not found\n" + x.toString)
+            }
+            case Left(x) => Left(x)
+          }
+        case Left(x) => Left(x)
+      }
+    case x => Left(x.toString)
+  }
+
+  def compileClassVarDec(lst: List[Syntax]): Either[String, (S_classVarDec, List[Syntax])] = lst match {
+    case (a@(S_keyword(_) | S_keyword(_))) :: xs => compileType(xs) match {
+      case Right((typ, S_ident(k) :: xs)) => compileVarNames(0, xs) match {
+        case (i, l) => {
+          val s = l.take(i)
+          l.drop(i) match {
+            case S_symbol(";") :: xs => Right((S_classVarDec(a.asInstanceOf[S_keyword], typ, S_ident(k) :: l), xs))
+            case x => Left(x.toString)
+          }
         }
       }
+      case Right(x) => Left(x.toString)
+      case Left(x) => Left("Require Type in var dec in class\n" + x.toString)
     }
+    case x => Left(x.toString)
   }
 
-  def compileClassVarDec(lst: List[Syntax]): List[Syntax] = lst match {
-    case
+  def compileSubroutine(lst: List[Syntax]): Either[String, (S_subroutineDec, List[Syntax])] = lst match {
+    case (a@(S_keyword("constructor") | S_keyword("function") | S_keyword("method"))) :: xs => xs match {
+      case xs => compileType(xs) match {
+        case Right((typ, S_ident(subname) :: S_symbol("(") :: xs)) => compileParametererList(xs) match {
+          // case Right((S_parameterList(l), S_symbol(")") :: xs)) =>
+          case Left(x) => Left(x)
+        }
+        case Left(S_keyword("void") :: S_ident(subname) ::  S_symbol("(") :: xs) => compileParametererList(xs) match {
+          // case Right((S_parameterList(l), S_symbol(")") :: xs)) =>
+          case Left(x) => Left(x)
+        }
+        case Left(x) => Left(x.toString())
+      }
+    }
+    case x => Left(x.toString)
   }
 
+  def compileParametererList(lst: List[Syntax]): Either[String, (S_parameterList, List[Syntax])] = compileType(lst) match {
+    case Right((typ, S_ident(vname) :: S_symbol(",") :: xs)) => compileParametererList(xs) match {
+      case Right((S_parameterList(l), xs)) => Right((S_parameterList((typ, S_ident(vname)) :: l), xs))
+      case Left(x) => Left(x)
+    }
+    case Right((typ, S_ident(vname) :: xs)) => Right((S_parameterList(List((typ, S_ident(vname)))), xs))
+    case Left(xs) => Right((S_parameterList(Nil), xs))
+  }
 
-  def compileSubroutine(lst: List[Syntax]): List[Syntax] = List()
-  def compileParametererList(lst: List[Syntax]): List[Syntax] = List()
   def compileVarDec(lst: List[Syntax]): List[Syntax] = List()
   def compileStatements(lst: List[Syntax]): List[Syntax] = List()
   def compileDo(lst: List[Syntax]): List[Syntax] = List()
@@ -48,4 +89,24 @@ class CompilationEngine(input: String) {
   def compileExpression(lst: List[Syntax]) : List[Syntax] = List()
   def compileTerm(lst: List[Syntax]): List[Syntax] = List()
   def compileExpressionList(lst: List[Syntax]): List[Syntax] = List()
+
+
+  // def compileSubRoutineBody(i: Int, lst: List[Syntax]): (Int, List[Syntax]) = lst match {
+
+  // }
+
+  def compileVarNames(i: Int, lst: List[Syntax]): (Int, List[Syntax]) = lst match {
+    case S_symbol(",") :: (s@S_symbol(_)) :: xs => {
+      compileVarNames(i + 1, xs) match { case (k, ss) =>  (k, s :: ss) }
+    }
+    case x => (i, x)
+  }
+
+  private def compileType(lst: List[Syntax]): Either[List[Syntax], (S_type, List[Syntax])] = lst match {
+    case S_keyword("int")     :: xs => Right((S_type("int"), xs))
+    case S_keyword("char")    :: xs => Right((S_type("char"), xs))
+    case S_keyword("boolean") :: xs => Right((S_type("boolean"), xs))
+    case S_ident(a)           :: xs => Right((S_type(a), xs))
+    case _ => Left(lst)
+  }
 }
--- books/the_elements_of_computing_systems/projects/tecs/src/main/scala/tecs/compiler/Syntax.scala
+++ books/the_elements_of_computing_systems/projects/tecs/src/main/scala/tecs/compiler/Syntax.scala
@@ -1,94 +1,14 @@
 package tecs.compiler
 
-class SyntaxBuilder {
-  // def build(tokens: List[TKN]) : S_class = tkn2syntax(tokens) match {
-  //   case S_class(c, cvd, sd) => S_class(c, cvd,sd)
-  //   case _ => throw new Exception("unknow in class")
-  // }
-
-  private def tkn2syntax(l : List[TKN]): List[Syntax] = l match {
-    case KEYWORD(k)      :: List() => List(S_keyword(k))
-    case SYMBOL(k)       :: List() => List(S_symbol(k))
-    case IDENTIFIER(k)   :: List() => List(S_ident(k))
-    case INT_CONST(k)    :: List() => List(S_intConst(k))
-    case STRING_CONST(k) :: List() => List(S_stringConst(k))
-    case KEYWORD(k)      :: xs => S_keyword(k)     :: tkn2syntax(xs)
-    case SYMBOL(k)       :: xs => S_symbol(k)      :: tkn2syntax(xs)
-    case IDENTIFIER(k)   :: xs => S_ident(k)       :: tkn2syntax(xs)
-    case INT_CONST(k)    :: xs => S_intConst(k)    :: tkn2syntax(xs)
-    case STRING_CONST(k) :: xs => S_stringConst(k) :: tkn2syntax(xs)
-  }
-}
-
-
 sealed trait Syntax
 
-case class S_class(
-  className: S_ident,
-  classVarDec: S_classVarDec,
-  subroutineDec: S_subroutineDec) extends Syntax
-
-case class S_classVarDec(
-  prefix: S_keyword,
-  tyep: S_ident,
-  varNames: List[S_varName],
-  s: List[Syntax]) extends Syntax
-
-// object S_classVarDec {
-//   def unapply(s: List[Syntax]):  Option[(S_keyword, S_ident, List[S_varName], List[Syntax])] =
-//     s match {
-//       case (sf@(S_keyword("static") | S_keyword("field"))) :: S_type(s, rest) => rest match {
-//         case (s@(S_ident(_))) :: xs => None
-//       }
-//       case _ => None
-//     }
-// }
-
+case class S_class(className: S_ident, classVarDec: S_classVarDec, subroutineDec: S_subroutineDec) extends Syntax
+case class S_classVarDec(prefix: S_keyword, t: S_type, varNames: List[Syntax]) extends Syntax
 case class S_subroutineDec(l: List[Syntax]) extends Syntax
-// object S_subroutineDec {
-//   def unapply(s: List[Syntax]):  Option[(S_keyword, S_ident, List[S_varName], List[Syntax])] = {
-//     None
-//   }
-// }
-
-
-case class S_varName(l: List[Syntax]) extends Syntax
-
-case class S_type(l: List[Syntax]) extends Syntax
-// object S_type {
-//   def unapply(s: List[Syntax]):  Option[(Syntax,  List[Syntax])] = s match {
-//     case (a@(S_keyword("int") | S_keyword("char") | S_keyword("boolean") | S_ident(_))) :: xs => Some((a, xs))
-//     case _ => None
-//   }
-// }
-
+case class S_parameterList(l: List[(S_type, S_ident)]) extends Syntax
+case class S_type(l: String) extends Syntax
 case class S_keyword(keyword: String) extends Syntax
 case class S_symbol(keyword: String) extends Syntax
 case class S_ident(keyword: String) extends Syntax
 case class S_intConst(keyword: String) extends Syntax
 case class S_stringConst(keyword: String) extends Syntax
-
-// val a = List(1, 2, 3, 4, 5)
-// a match {
-//   case A1(i) => println(i)
-// }
-
-// trait A
-
-// case class A1(i: List[Int]) extends A
-// object A1 {
-//   def unapply(l : List[Int]): Option[List[Int]] = l match {
-//     case x :: y :: xs => xs match { case A2(i) => Some(x :: y :: i) }
-//     case x :: y => Some(x :: y)
-//     case _ => Some(List())
-//   }
-// }
-
-// case class A2(l: List[Int]) extends A
-// object A2 {
-//   def unapply(l : List[Int]): Option[List[Int]] = l match {
-//     case x :: y :: xs => xs match { case A2(i) => Some(x :: y :: i) }
-//     case x :: y => Some(x :: y)
-//     case _ => Some(List())
-//   }
-// }
