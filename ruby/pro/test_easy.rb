d96318e65eaaf0b7c1743f9cef19303c04a9c2fe
ganmacs <ganmacs@gmail.com>
2016-02-16 23:51:26 +0900

    easy

--- ruby/pro/test_easy.rb
+++ ruby/pro/test_easy.rb
@@ -13,33 +13,67 @@ class A
 end
 
 class Worker
-  def initialize
-    @read_pipe, @write_pipe = IO.pipe
+  def initialize(&block)
+    @child_read, @parent_write = IO.pipe
+    @parent_read, @child_write = IO.pipe
+    @block = block
   end
 
   def run
     @pid = fork do
-      @read_pipe.close
+      close_child_pipe
 
-      a = A.new(1, 2, 3)
-      write_obj(a)
+      r = read_from_parent
+      r2 = @block.call(r)
+      write_to_parent(r2)
 
-      @write_pipe.close
+      close_parent_pipe
     end
 
-    puts read_obj
-    @read_pipe.close
+    write_to_child(A.new(1, 2, 3))
+    r = read_from_child
+    puts r
   end
 
-  def write_obj(obj)
+  def close_child_pipe
+    @child_read.close
+    @child_write.close
+  end
+
+  def close_parent_pipe
+    @parent_write.close
+    @parent_read.close
+  end
+
+  def write_obj(obj, pipe)
     data = Marshal.dump(obj)
-    @write_pipe.puts data
+    pipe.puts data
   end
 
-  def read_obj
-    data = @read_pipe.gets.chomp
+  def write_to_parent(obj)
+    write_obj(obj, @parent_write)
+  end
+
+  def write_to_child(obj)
+    write_obj(obj, @child_write)
+  end
+
+  def read_obj(pipe)
+    data = pipe.gets.chomp
     Marshal.load(data)
+  end
+
+  def read_from_parent
+    read_obj(@parent_read)
+  end
+
+  def read_from_child
+    read_obj(@child_read)
   end
 end
 
-Worker.new.run
+Worker.new do |x|
+  puts "child process"
+  sleep(2)                 # blocking
+  A.new(1, 2, x)
+end.run
