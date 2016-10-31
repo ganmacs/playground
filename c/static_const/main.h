f3a6abf4e8c8396aeff348543735011cec9b60bf
ganmacs <ganmacs@gmail.com>
2016-10-31 13:25:56 +0900

    fix static const

--- c/static_const/main.h
+++ c/static_const/main.h
@@ -1,13 +1,14 @@
 #ifndef H1
 #define H1
-
 #include <stdio.h>
-#include "debug.h"
-#include "debug2.h"
 
 typedef struct obj_t {
   int value;
 } obj_t;
 
-static obj_t *NIL = &(obj_t) { 1 };
+obj_t *NIL = &(obj_t) { 1 };
+
+#include "debug.h"
+#include "debug2.h"
+
 #endif
