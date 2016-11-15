5ef7e09bc9c74f6ef6297f5fd4a3c8182adc2076
ganmacs <ganmacs@gmail.com>
2016-11-15 18:31:27 +0900

    update

--- books/pc_data_structure/15/articulation_point.cpp
+++ books/pc_data_structure/15/articulation_point.cpp
@@ -2,14 +2,16 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
 const int MAX = 100000;
 bool vist[MAX];
 int n, v;
-
+std::set<int> S;
 vector<int> G[MAX];
 int prenum[MAX], lowest[MAX];
+int tt = 0;
 
 int dfs(int current, int t)
 {
@@ -17,25 +19,37 @@ int dfs(int current, int t)
   t++;
   vist[current] = true;
 
-
   for (int i = 0; i < (int)G[current].size(); i++) {
     int next = G[current][i];
-    if (!vist[v]) {
+    if (!vist[next]) {
       t = dfs(next, t);
-      // lowest[current] = min(lowest[current], lowest[next]);
+
+      if (prenum[current] <= lowest[next] && G[current].size() > 1) {
+        if (current == 0) tt++;
+        if (current == 0 && tt > 1) {
+          S.insert(0);
+        } else if(current != 0){
+          S.insert(current);
+        }
+
+      }
+      lowest[current] = min(lowest[current], lowest[next]);
     } else {
-      // need prev?
+      lowest[current] = min(lowest[current], prenum[next]);
     }
-
-
   }
   return t;
 }
 
 void ap()
 {
-
+  tt = 0;
   dfs(0, 0);
+
+  for (set<int>::iterator it = S.begin();it != S.end();it++) {
+    cout << *it << endl;
+  }
+
 }
 
 int main()
@@ -48,7 +62,6 @@ int main()
     G[s].push_back(t);
     G[t].push_back(s);
   }
-
 
   for (int i = 0; i < n; i++) {
     vist[i] = false;
