614ebf0d315a87fb1c8ecb9c598528e03af6e84d
ganmacs <ganmacs@gmail.com>
2016-11-26 23:14:48 +0900

    1

---
+++ books/pc_data_structure/appendix/icpc_ranking.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <set>
+
+using namespace std;
+static const int MAX = 2000;
+
+struct team { int i, count, duration, miss_count; };
+
+team tt[MAX + 1];
+int n;
+int M, T, P, R;
+int m, t, p, j;
+
+bool msort(const team &t1, const team &t2)
+{
+  if (t1.count == t2.count) {
+    if (t1.duration == t2.duration) {
+      return t1.i > t2.i;       // index
+    }
+    return t1.duration < t2.duration;
+  }
+
+  return t1.count > t2.count;
+}
+
+int main(){
+  cin >> M >> T >> P >> R;
+
+  while (!(M == 0 && T == 0 && P == 0 && R == 0)) {
+    for (int i = 1; i <= T; i++) {
+      tt[i].i = i;
+      tt[i].count = 0;
+      tt[i].duration = 0;
+      tt[i].miss_count = 0;
+    }
+
+    for (int i = 0; i < R; i++) {
+      cin >> m >> t >> p >> j;
+      if (j == 0) {
+        tt[t].count++;
+        tt[t].duration += (m + (20 * tt[t].miss_count));
+        tt[t].miss_count = 0;
+      } else {
+        tt[t].miss_count++;
+      }
+    }
+
+    sort(tt + 1, tt + (T + 1), msort);
+
+    for (int i = 1; i <= T; i++) {
+      cout << tt[i].i;
+      if (i == T) continue;
+
+      if (tt[i].count == tt[i + 1].count && tt[i].duration == tt[i + 1].duration) {
+        cout << "=";
+      } else {
+        cout << ",";
+      }
+    }
+    cout << endl;
+
+    cin >> M >> T >> P >> R;
+  }
+}
