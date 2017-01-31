7e59a831623971c5f5e7246cdf7c2903cdbe8b98
ganmacs <ganmacs@gmail.com>
2017-01-31 11:48:55 +0900

    update

--- rust/llvm-example/src/main.rs
+++ rust/llvm-example/src/main.rs
@@ -3,7 +3,7 @@ extern crate llvm_example;
 use llvm_example::*;
 use llvm_example::node::*;
 
-fn main() {
+fn test1() {
     let v1 = assgn("foo", iint(11));
     let v3 = assgn("foo", sub(aref("foo"), iint(10)));
     let i = ifexpr(aref("foo"),
@@ -12,4 +12,15 @@ fn main() {
     let ret = aref("foo");
 
     compile(vec![v1, v3, i, ret]);
+}
+
+fn test2() {
+    unsafe {
+        codegen::playground();
+    }
+}
+
+fn main() {
+    // test1()
+    test2()
 }
