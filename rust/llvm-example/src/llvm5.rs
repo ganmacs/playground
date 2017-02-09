6ff829220f094b9cc717d78150f7f897f9fdce27
ganmacs <ganmacs@gmail.com>
2017-02-09 15:52:22 +0900

    define function and call function

--- rust/llvm-example/src/llvm5.rs
+++ rust/llvm-example/src/llvm5.rs
@@ -19,17 +19,24 @@ pub unsafe fn run() {
     let module = llvm::core::LLVMModuleCreateWithNameInContext(mod_name.as_ptr(), context);
     let builder = llvm::core::LLVMCreateBuilderInContext(context);
 
-    // build a function named "main"
+    // build "i64 main" function
     let int_type = llvm::core::LLVMInt64TypeInContext(context);
     let fun_type = llvm::core::LLVMFunctionType(int_type, ptr::null_mut(), 0, 0);
     let fun_name = CString::new("main").unwrap();
-    let fun = llvm::core::LLVMAddFunction(module, fun_name.as_ptr(), fun_type);
+    let main_fun = llvm::core::LLVMAddFunction(module, fun_name.as_ptr(), fun_type);
 
-    // create basic block named "entry"
+    // build "i64 inc(i64)" function
+    let int_type = llvm::core::LLVMInt64TypeInContext(context);
+    let arg_types = &mut [int_type];
+    let fun_type = llvm::core::LLVMFunctionType(int_type, arg_types.as_mut_ptr(), 1, 0);
+    let fun_name = CString::new("inc").unwrap();
+    let inc_fun = llvm::core::LLVMAddFunction(module, fun_name.as_ptr(), fun_type);
+
+    // create "entry" basic block in "main" function
     let bb_name = CString::new("entry").unwrap();
-    let bb = llvm::core::LLVMAppendBasicBlockInContext(context, fun, bb_name.as_ptr());
+    let bb = llvm::core::LLVMAppendBasicBlockInContext(context, main_fun, bb_name.as_ptr());
 
-    // Set next insert positoin
+    // Set next insert position
     llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
 
     // allocate `x` on stack (memory)
@@ -37,36 +44,47 @@ pub unsafe fn run() {
     let cname = CString::new("x").unwrap();
     let pointer_a = llvm::core::LLVMBuildAlloca(builder, int_type, cname.as_ptr());
 
-    // store 2 to `x`. `x` is a pointer.
+    // store 2 to `x`.
     let typ = llvm::core::LLVMInt64TypeInContext(context);
     let iv = llvm::core::LLVMConstInt(typ, 2, 0);
     llvm::core::LLVMBuildStore(builder, iv, pointer_a);
 
-    // load a to a_value (register)
+    // load a to x (register)
     let name = CString::new("x").unwrap();
     let new_a = llvm::core::LLVMBuildLoad(builder, pointer_a, name.as_ptr());
 
-    // build a function named "inc"
-    let int_type = llvm::core::LLVMInt64TypeInContext(context);
-    let vk = &mut [int_type];
-    let fun_type = llvm::core::LLVMFunctionType(int_type, vk.as_mut_ptr(), 1, 0);
-    let fun_name = CString::new("inc").unwrap();
-    let fun = llvm::core::LLVMAddFunction(module, fun_name.as_ptr(), fun_type);
+    // call inc(x)
+    let args = &mut [new_a];
+    let cname = CString::new("ret").unwrap();
+    let ret = llvm::core::LLVMBuildCall(builder,
+                                        inc_fun,
+                                        args.as_mut_ptr(),
+                                        args.len() as u32,
+                                        cname.as_ptr());
+
+    // return ret
+    llvm::core::LLVMBuildRet(builder, ret);
 
     // create basic block named "entry"
     let bb_name = CString::new("entry").unwrap();
-    let inc_bb = llvm::core::LLVMAppendBasicBlockInContext(context, fun, bb_name.as_ptr());
+    let inc_bb = llvm::core::LLVMAppendBasicBlockInContext(context, inc_fun, bb_name.as_ptr());
 
     // Set next insert positoin
     llvm::core::LLVMPositionBuilderAtEnd(builder, inc_bb);
 
+    // load 1st arguments
+    let lh = llvm::core::LLVMGetParam(inc_fun, 0);
+
+    // create const value 1
     let typ = llvm::core::LLVMInt64TypeInContext(context);
-    let iv = llvm::core::LLVMConstInt(typ, 2, 0);
-    let vv = &mut [iv];
+    let rh = llvm::core::LLVMConstInt(typ, 1, 0);
 
-    let cname = CString::new("what").unwrap();
-    llvm::core::LLVMBuildCall(builder, fun, vv.as_mut_ptr(), 1, cname.as_ptr());
+    // 1 + 0th argument
+    let name = CString::new("add_value").unwrap();
+    let ret = llvm::core::LLVMBuildAdd(builder, lh, rh, name.as_ptr());
 
+    // return value
+    llvm::core::LLVMBuildRet(builder, ret);
 
     // dump code both stdout and file
     llvm::core::LLVMDumpModule(module);
