09b5d1bfa1d2691d7e88d200e20117bb1aaff08c
ganmacs <ganmacs@gmail.com>
2017-04-16 16:36:43 +0900

    Add performance measuring code

--- go/striped/examples/main.go
+++ go/striped/examples/main.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/ganmacs/playground/go/striped"
 )
 
-func main() {
-	fmt.Println("hgoe")
+type Dict interface {
+	Get(string) int
+	Set(string, int)
+}
+
+func createSampleMap(n int) map[string]int {
+	m := make(map[string]int)
+	k := ""
+
+	for i := 0; i < n; i++ {
+		k = fmt.Sprintf("%d", i)
+		m[k] = i
+	}
+
+	return m
+}
+
+func test1() {
+	doneChan := make(chan int)
+	m := createSampleMap(100000)
+	// dict := striped.NewSynchronizedDict()
 	dict := striped.NewStripedDict()
 
-	dict.Set("foo", 1)
-	fmt.Println(dict.Get("foo"))
+	startTime := time.Now()
+	go dictWriter(doneChan, dict, m)
+	go dictWriter(doneChan, dict, m)
+	go dictWriter(doneChan, dict, m)
+	go dictWriter(doneChan, dict, m)
+	go dictWriter(doneChan, dict, m)
+
+	<-doneChan
+	<-doneChan
+	<-doneChan
+	<-doneChan
+	<-doneChan
+
+	fmt.Println("Elapsed Time in test1:", time.Since(startTime))
+}
+
+func dictWriter(done chan int, d Dict, sample map[string]int) {
+	for k, v := range sample {
+		d.Set(k, v)
+	}
+
+	done <- 0
+}
+
+func main() {
+	test1()
 }
