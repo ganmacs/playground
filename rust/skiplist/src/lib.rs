a5a622e8a62a120a2397baeb2ad203b0dd039b87
ganmacs <ganmacs@gmail.com>
2018-03-04 12:45:56 +0900

    make MemDB iteratable

--- rust/skiplist/src/lib.rs
+++ rust/skiplist/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate bytes;
 extern crate rand;
 
+use std::iter::{Iterator, IntoIterator};
 mod node_arena;
 mod node;
 mod fixed_list;
@@ -28,20 +29,97 @@ impl MemDB {
 
     // Should be LookupKey
     pub fn get(&self, key: &InternalKey) -> Option<Bytes> {
-        let v = key.memtable_key();
+        // let v = key.memtable_key();
+        let v = key.user_key();
         self.inner.get(&v)
     }
 
-    pub fn add(&mut self, key_kind: KeyKind, ukey: Bytes, value: Bytes) {
+    pub fn add(&mut self, key_kind: KeyKind, ukey: &str, value: &Bytes) {
         let n = ukey.len();
         let mut buf = [0; 4];
         LittleEndian::write_u32(&mut buf, n as u32);
-        let mut key = Bytes::from(buf.to_vec());
-        key.extend(ukey);
+        // let mut key = Bytes::from(buf.to_vec());
+        // key.extend(Bytes::from(ukey));
 
         match key_kind {
-            KeyKind::SET => self.inner.insert(&key, &value),
+            KeyKind::SET => self.inner.insert(&Bytes::from(ukey), &value),
             KeyKind::DELETE => (),    // TODO
         };
+    }
+}
+
+impl IntoIterator for MemDB {
+    type Item = (Bytes, Bytes);
+    type IntoIter = MemDBIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        MemDBIterator { inner: self.inner.into_iter() }
+    }
+}
+
+pub struct MemDBIterator {
+    inner: skiplist::SkipListIterator,
+}
+
+impl Iterator for MemDBIterator {
+    type Item = (Bytes, Bytes);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{MemDB, Bytes, InternalKey, KeyKind};
+
+    #[test]
+    fn test_skiplist() {
+        let mut db = MemDB::new();
+
+        let hash = vec![
+            ("key", Bytes::from("value")),
+            ("key1", Bytes::from("value1")),
+            ("key2", Bytes::from("value2")),
+            ("key3", Bytes::from("value3")),
+            ("key4", Bytes::from("value4")),
+            ("key5", Bytes::from("value5")),
+            ("key6", Bytes::from("value___6")),
+            ("key77", Bytes::from("value   7")),
+        ];
+
+        for v in hash {
+            db.add(KeyKind::SET, &v.0, &v.1);
+            assert_eq!(db.get(&InternalKey::new(&v.0, 0)).unwrap(), v.1);
+        }
+
+        assert_eq!(db.get(&InternalKey::new("notfound", 0)), None);
+    }
+
+
+    #[test]
+    fn test_skiplist_iter() {
+        let mut db = MemDB::new();
+
+        let mut hash: Vec<(&str, Bytes)> = vec![
+            ("key", Bytes::from("value")),
+            ("key1", Bytes::from("value1")),
+            ("key2", Bytes::from("value2")),
+            ("key3", Bytes::from("value3")),
+            ("key4", Bytes::from("value4")),
+            ("key5", Bytes::from("value5")),
+            ("key6", Bytes::from("value___6")),
+            ("key77", Bytes::from("value   7")),
+        ];
+
+        for v in &hash.clone() {
+            db.add(KeyKind::SET, v.0, &v.1);
+        }
+
+        let mut it = db.into_iter();
+        for v in hash.into_iter() {
+            assert_eq!(it.next().unwrap(), (Bytes::from(v.0), v.1));
+        }
+
     }
 }
--- rust/skiplist/src/skiplist.rs
+++ rust/skiplist/src/skiplist.rs
@@ -207,6 +207,55 @@ impl SkipIndex {
     }
 }
 
+pub struct SkipListIterator {
+    pos: usize,
+    idx: SkipIndex,
+    inner: Bytes,
+}
+
+impl Iterator for SkipListIterator {
+    type Item = (Key, Value);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pos = self.next_0();
+        if self.pos == HEAD {
+            return None;
+        }
+
+        let vi = self.idx.val(self.pos);
+        let ki = self.idx.key(self.pos);
+        Some((self.load(ki), self.load(vi)))
+    }
+}
+
+
+impl SkipListIterator {
+    fn next_0(&self) -> usize {
+        self.idx.next(self.pos)
+    }
+
+    // TODO
+    fn load(&self, i: usize) -> Bytes {
+        let u64_offset = i + 8;
+        let buf = self.inner.slice(i, u64_offset);
+        let v = LittleEndian::read_u64(&buf) as usize;
+        self.inner.slice(u64_offset, u64_offset + v)
+    }
+}
+
+impl IntoIterator for SkipList {
+    type Item = (Key, Value);
+    type IntoIter = SkipListIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        SkipListIterator {
+            pos: 0,
+            idx: self.idx,
+            inner: self.data,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::{SkipList, Bytes};
@@ -232,5 +281,36 @@ mod tests {
         }
 
         assert_eq!(list.get(&Bytes::from("notfound")), None);
+    }
+
+    #[test]
+    fn test_skiplist_iter() {
+        let mut list = SkipList::new();
+
+        let hash = vec![
+            (Bytes::from("key"), Bytes::from("value")),
+            (Bytes::from("key1"), Bytes::from("value1")),
+            (Bytes::from("key2"), Bytes::from("value2")),
+            (Bytes::from("key3"), Bytes::from("value3")),
+            (Bytes::from("key4"), Bytes::from("value4")),
+            (Bytes::from("key5"), Bytes::from("value5")),
+            (Bytes::from("key6"), Bytes::from("value___6")),
+            (Bytes::from("key77"), Bytes::from("value   7")),
+        ];
+
+        for v in &hash.clone() {
+            list.insert(&v.0, &v.1);
+        }
+
+        let mut it = list.into_iter();
+        assert_eq!(it.next().as_ref(), hash.get(0));
+        assert_eq!(it.next().as_ref(), hash.get(1));
+        assert_eq!(it.next().as_ref(), hash.get(2));
+        assert_eq!(it.next().as_ref(), hash.get(3));
+        assert_eq!(it.next().as_ref(), hash.get(4));
+        assert_eq!(it.next().as_ref(), hash.get(5));
+        assert_eq!(it.next().as_ref(), hash.get(6));
+        assert_eq!(it.next().as_ref(), hash.get(7));
+        assert_eq!(it.next().as_ref(), None);
     }
 }
