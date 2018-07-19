4605191f67fc71acbc8fe4cb64b2aa9946c104fe
ganmacs <ganmacs@gmail.com>
2018-07-19 18:24:38 +0900

    simple

--- cpp/tcp/main.cc
+++ cpp/tcp/main.cc
@@ -3,8 +3,9 @@
 #include "event2/buffer.h"
 #include "event2/event.h"
 #include <iostream>
+#include <algorithm>
+#include <string>
 
-#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -35,6 +36,11 @@ static_assert(offsetof(RawSlice, len_) == offsetof(evbuffer_iovec, iov_len),
               "RawSlice != evbuffer_iovec");
 
 
+static void dummyHttp(int fd)  {
+    std::string header {"HTTP1.1 200 OK\nContent-Type: text/plain\n\nbody"};
+    ::write(fd, header.c_str(), header.size());
+}
+
 class Socket {
 public:
     Socket(int fd): fd_{fd} {}
@@ -48,67 +54,80 @@ private:
 
 class Buffer {
 public:
-    Buffer(int fd): fd_{fd}, buffer_(evbuffer_new()) {}
+    Buffer(): buffer_{evbuffer_new()} {}
     // ~Buffer() { evbuffer_free(buffer_); }
 
-    int read(uint64_t max_length) {
+    int read(const int fd, const uint64_t max_length) {
+        puts("-- read! --");
         const uint64_t MaxSlices = 2;
         RawSlice slices[MaxSlices];
         const uint64_t num_slices = reserve(max_length, slices, MaxSlices);
-
         iovec iov[num_slices];
         uint64_t num_bytes_to_read = 0;
-        // TODO
+
         uint64_t i = 0;
+        // TODO
         for (; i < num_slices; i++) {
-            printf("1%d\n", fd_);
             iov[i].iov_base = slices[i].mem_;
-            printf("2%d\n", fd_);
-            // const size_t len = std::min(slices[i].len_, static_cast<size_t>(max_length - num_bytes_to_read));
-            printf("3%d\n", fd_);
-            iov[i].iov_len = max_length;
-            printf("%d\n", fd_);
+            const size_t len = std::min(slices[i].len_, static_cast<size_t>(max_length - num_bytes_to_read));
+            iov[i].iov_len = len;
             num_bytes_to_read += max_length;
-            printf("%d\n", fd_);
         }
 
-        char buf[5];
-        memset(buf, 0, sizeof(buf));
-        int n = ::read(fd_, buf, sizeof(buf));
-        printf("%d %s\n",n,  buf);
+        const ssize_t rc = ::readv(fd, iov, static_cast<int>(i));
+        if (rc < 0) {
+            // TODO: if EAGAIN, then more data would read.
+            return rc;
+        }
 
+        uint64_t num_to_commit = rc;
+        uint64_t j = 0;
+        while (num_to_commit > 0) {
+            slices[j].len_ = std::min(slices[j].len_, static_cast<size_t>(num_to_commit));
+            num_to_commit -= slices[j].len_;
+            j++;
+        }
 
+        commit(slices, j);
+        return rc;
+    }
 
-        const ssize_t rc = ::readv(fd_, iov, static_cast<int>(i));
-        if (rc < -0) {
-            printf("%ld\n", rc);
+    void commit(RawSlice* iovecs, uint64_t const size) {
+        int ret = evbuffer_commit_space(buffer_, reinterpret_cast<evbuffer_iovec*>(iovecs), size);
+        if (ret != 0) {
+            puts("evbuffer_commit_sapce failed");
             exit(1);
         }
-
-        return 0;
     }
-private:
 
-    uint64_t reserve(uint64_t length, RawSlice* iovecs, uint64_t iovecs_num) {
+    uint64_t getAvailableSliceCount()  {
+        return getRawSlice(nullptr, 0);
+    }
+    uint64_t getRawSlice(RawSlice* out, uint64_t size)  {
+        return evbuffer_peek(buffer_, -1, nullptr, reinterpret_cast<evbuffer_iovec*>(out), size);
+    }
+
+    evbuffer* buffer() { return buffer_; }
+private:
+    uint64_t reserve(uint64_t const length, RawSlice* iovecs, uint64_t const iovecs_num) {
         int ret = evbuffer_reserve_space(buffer_, length, reinterpret_cast<evbuffer_iovec*>(iovecs), iovecs_num);
+        if (ret < 0) {
+            printf("%d\n", ret);
+            exit(1);
+        }
         return ret;
     }
 
-    int fd_;
     evbuffer* buffer_;
 };
 
 class Connection {
 public:
-    Connection(int fd): fd_{fd} {
-        Buffer b {fd};
-        buffer_ = &b;
-    }
-
-    Buffer* buffer() { return buffer_; }
+    Connection(): buffer_{} {}
+    Buffer* buffer() { return &buffer_; }
 private:
-    int fd_;
-    Buffer* buffer_;
+    // int fd_;
+    Buffer buffer_;
 };
 
 class Server {
@@ -157,9 +176,37 @@ private:
 void Server::listenCallback(evconnlistener*, evutil_socket_t fd, sockaddr* remote_addr, int remote_addr_len, void* arg) {
     Server* server = static_cast<Server*>(arg);
     printf("received %d %d\n", fd, server->fd());
-    Connection conn {fd};
-    conn.buffer()->read(65533);
+    Connection conn {};
+
+    uint64_t bytes_read = 0;
+    while(true) {
+        int rc = conn.buffer()->read(fd, 65535);
+
+        if (rc == 0) {
+            puts("finish!");
+            break;
+        } else if (rc < 0){
+            puts("failed?");
+            break;
+            // exit(1);
+        } else {
+            bytes_read += rc;
+        }
+    }
+
+    uint64_t size = conn.buffer()->getAvailableSliceCount();
+    RawSlice slices[size];
+    conn.buffer()->getRawSlice(slices, size);
+
+    for (auto s : slices) {
+        char* v = static_cast<char*>(s.mem_);
+        printf("%s\n", v);
+    }
+
+    dummyHttp(fd);
+    close(fd);
 }
+
 
 
 int main(int argc, char *argv[])
