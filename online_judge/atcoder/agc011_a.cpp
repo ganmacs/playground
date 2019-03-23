dd29df2fad7c592d2cb072822bc95f92d2dce5a7
Yuta Iwama <ganmacs@gmail.com>
2019-03-23 14:20:57 +0900

    7

--- online_judge/atcoder/agc011_a.cpp
+++ online_judge/atcoder/agc011_a.cpp
@@ -19,23 +19,17 @@ int main(int argc, char *argv[])
   for (int i = 0; i < N; i++) {
     cin >> T[i];
   }
-  sort(T, T + N, greater<int>());
+  sort(T, T + N);
 
-  int ans = 0;
-  int i = 0;
-  while (i < N) {
-    int sumv = 0;
-    long long t = max(T[i] - K, 0LL);
-    printf("a %d %lld\n",i , T[i]);
-
-    while (i + 1 < N && T[i + 1] >= t && sumv < C) {
-      i++;
-      sumv++;
+  int ans = 0, away = -1, cnt = 0;
+  for (int i = 0; i < N; i++) {
+    if (cnt == C || away < T[i]) {
+      ans++;
+      cnt = 1;
+      away = T[i] + K;
+    } else {
+      cnt++;
     }
-    printf("b %d %d\n", i, sumv);
-
-    if (sumv == 0) i++;
-    ans++;
   }
 
   printf("%d\n", ans);
